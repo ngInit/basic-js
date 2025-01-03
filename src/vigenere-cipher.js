8d52ed3451581475cc3edc176967b532c7f1f18f
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2025-01-03 23:38:50 +0100

    feat: solve VigenereCipheringMachine in vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,69 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(isDirectText = true) {
+        this.isDirectText = isDirectText;
+    }
+
+    cipheringMachine(text, key, action) {
+        let index = 0;
+        let shift = 0;
+        let result = '';
+        text.split('').map((item) => {
+            if (index === key.length) {
+                index = 0;
+            }
+            const shifter = key[index].charCodeAt(0) - 65;
+            if (item.charCodeAt(0) > 64 && item.charCodeAt(0) < 91) {
+                if (action === 'encrypt') {
+                    shift = item.charCodeAt(0) + shifter;
+                    if (shift >= 91) {
+                        shift = (shift - 91) + 65;
+                    }
+                }
+                if (action === 'decrypt') {
+                    shift = item.charCodeAt(0) - shifter;
+                    if (shift < 65) {
+                        shift = (shift + 91) - 65;
+                    }
+                }
+                result += String.fromCharCode(shift);
+                index += 1;
+            } else {
+                result += item;
+            }
+        })
+       return this.reverseText(result);
+    }
+
+    reverseText(text) {
+        return this.isDirectText ?
+            text :
+            text.split('').reverse().join('');
+    }
+
+    checkData(text, key) {
+        if (!text || !key) {
+            throw new Error("Incorrect arguments!");
+        }
+        return true;
+    }
+
+    encrypt(text, key) {
+        if (this.checkData(text, key)) {
+            text = text.toUpperCase();
+            key = key.toUpperCase();
+            return this.cipheringMachine(text, key, 'encrypt');
+        }
+    }
+
+    decrypt(text, key) {
+        if (this.checkData(text, key)) {
+            text = text.toUpperCase();
+            key = key.toUpperCase();
+            return this.cipheringMachine(text, key, 'decrypt');
+        }
+    }
 }
 
 module.exports = {
